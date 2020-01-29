9d2128e28a0ca7855ccf719e3651435125723483
Caio Augusto Papai <cpapai@uolinc.com>
2020-01-29 10:49:33 -0300

    escrevendo arquivo

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	simple "github.com/caiopapai/go-protocol-buffer-study/src"
+	"github.com/golang/protobuf/proto"
 )
 
 func main() {
-	doSimple()
+	sm := doSimple()
+	writeToFile("simple.bin", sm)
 }
 
-func doSimple() {
-	sa := simple.SimpleMessage{
+func writeToFile(fname string, pb proto.Message) error {
+	out, err := proto.Marshal(pb)
+	if err != nil {
+		log.Fatal("Error ", err)
+		return err
+	}
+
+	if err := ioutil.WriteFile(fname, out, 0644); err != nil {
+		log.Fatal("Erro ", err)
+		return err
+	}
+
+	fmt.Println("Written")
+	return nil
+}
+
+func doSimple() *simple.SimpleMessage {
+	sm := simple.SimpleMessage{
 		Id:         12345,
 		Name:       "Caio Papai",
 		IsSimple:   true,
 		SimpleList: []int32{4, 5, 6, 7},
 	}
 
-	fmt.Println("Mensagem: ", sa)
+	fmt.Println("Mensagem: ", sm)
+
+	return &sm
 }
